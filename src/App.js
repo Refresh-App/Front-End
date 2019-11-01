2e321b1278791a618f9b0b481a6bf853781bcbbd
roman-jordan <rjordan1352@gmail.com>
2019-10-31 18:46:45 -0700

    TEST 3 WEBHOOKS

--- src/App.js
+++ src/App.js
@@ -6,14 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+       
          
         </a>
       </header>
