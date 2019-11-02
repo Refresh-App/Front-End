abfe1c90f9317e2fe9d26ca3f27d90b40875dd92
roman-jordan <rjordan1352@gmail.com>
2019-11-02 10:09:08 -0700

    Messing with Call Back URL

--- src/components/signIn/GithubSignInButton.js
+++ src/components/signIn/GithubSignInButton.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 
  const GithubSignInButton = () =>{
-    return <a href="https://github.com/login/oauth/authorize?client_id=b9305af8a8b0ff6e76e9&redirect_uri=https://apidevnow.com/gitAuth">Login with github</a>
+    return <a href="https://github.com/login/oauth/authorize?client_id=b9305af8a8b0ff6e76e9">Login with github</a>
 }
 
 export default GithubSignInButton
