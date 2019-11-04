4a706c1c1e042a931fb9d5be5554cf62a587e4aa
roman-jordan <rjordan1352@gmail.com>
2019-11-03 19:39:49 -0800

    Yo

--- src/components/signIn/GithubSignInButton.js
+++ src/components/signIn/GithubSignInButton.js
@@ -7,7 +7,6 @@ const GithubSignInButton = () => {
       <a href="https://github.com/login/oauth/authorize?client_id=f248d7c7a624257a6899">
         Login with github
       </a>
-      <iframe src="https://apidevnow.com/gitAuth" />
     </>
   );
 };
