3f6d1456063c77909b852cdf7b227313e9a60910
roman-jordan <rjordan1352@gmail.com>
2019-11-03 19:51:50 -0800

    Yo

--- src/components/signIn/GithubSignInButton.js
+++ src/components/signIn/GithubSignInButton.js
@@ -1,7 +1,11 @@
 //Sign in Canvas
 import React from "react";
+import axios from 'axios'
 
 const GithubSignInButton = () => {
+    axios.get('https://apidevnow.com/gitAuth')
+    .then(res =>console.log(res))
+    .catch(res =>console.log(res))
   return (
     <>
       <a href="https://github.com/login/oauth/authorize?client_id=f248d7c7a624257a6899">
