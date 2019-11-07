4dedb19893a08b50ad0892124a15e5f55ff95d1b
roman-jordan <rjordan1352@gmail.com>
2019-11-07 14:00:24 -0800

    yo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-
-import * as serviceWorker from './serviceWorker';
-
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-
-    <Router>
-      <App />
-    </Router>,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById("root")
 );
 
