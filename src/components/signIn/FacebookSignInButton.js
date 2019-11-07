0f59b3a1ff4510761c7e3b3b05b11860e3a6ea15
roman-jordan <rjordan1352@gmail.com>
2019-11-07 12:47:05 -0800

    Fixing Error

--- src/components/signIn/FacebookSignInButton.js
+++ src/components/signIn/FacebookSignInButton.js
@@ -6,7 +6,7 @@ const FacebookSignInButton = () => {
   console.log('called')
   //You will notice the functions auth and authSuccess in each SignInButton, Maybe abstract out. 
   const auth = () =>{
-    window.open("https://refresh-yo.herokuapp.com/facebookAuth", "Sign In With Facebook ;)", "width=400,height=500")
+    window.open("https://apidevnow.com/facebookAuth", "Sign In With Facebook ;)", "width=400,height=500")
     window.addEventListener('message', response =>{
       authSuccess(response.data); // e.data hold the message
     } , false);
