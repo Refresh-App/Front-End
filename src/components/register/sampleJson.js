61af392a68b105d6b456bda69adb3c76572f5758
roman-jordan <rjordan1352@gmail.com>
2019-11-08 11:25:47 -0800

    checking staging

--- src/components/register/sampleJson.js
+++ src/components/register/sampleJson.js
@@ -1,6 +1,7 @@
 export const gitAuth = {
   avatar_url: "https://avatars2.githubusercontent.com/u/17465565?v=4",
-  bio: "Hi everyone, I am John. I pretty much love building stuff from the ground up, or, working on existing projects; though the former is my favorite. bool",
+  bio:
+    "Hi everyone, I am John. I pretty much love building stuff from the ground up, or, working on existing projects; though the former is my favorite. bool",
   blog: "http://www.romanjoran.tk",
   company: "@lambdaschools -- Student",
   created_at: "2016-02-25T03:01:29Z",
@@ -9,7 +10,8 @@ export const gitAuth = {
   followers: 30,
   followers_url: "https://api.github.com/users/John-Smith/followers",
   following: 37,
-  following_url: "https://api.github.com/users/John-Smith/following{/other_user}",
+  following_url:
+    "https://api.github.com/users/John-Smith/following{/other_user}",
   gists_url: "https://api.github.com/users/John-Smith/gists{/gist_id}",
   gravatar_id: "",
   hireable: true,
@@ -22,7 +24,8 @@ export const gitAuth = {
   organizations_url: "https://api.github.com/users/John-Smith/orgs",
   public_gists: 4,
   public_repos: 107,
-  received_events_url: "https://api.github.com/users/John-Smith/received_events",
+  received_events_url:
+    "https://api.github.com/users/John-Smith/received_events",
   repos_url: "https://api.github.com/users/John-Smith/repos",
   site_admin: false,
   starred_url: "https://api.github.com/users/John-Smith/starred{/owner}{/repo}",
@@ -33,13 +36,30 @@ export const gitAuth = {
   url: "https://api.github.com/users/John-Smith"
 };
 
-export const googleAuth ={
-    email: "romanjordan255@gmail.com",
-    email_verified: true,
-    family_name: "Smith",
-    given_name: "John",
-    locale: "en",
-    name: "John Smith",
-    picture: "https://lh3.googleusercontent.com/-ASaY0SANelo/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rdokkdjrZohTqzdndENxux0gLpnug/photo.jpg",
-    sub: "118064446743336738788"
-}
+export const googleAuth = {
+  email: "romanjordan255@gmail.com",
+  email_verified: true,
+  family_name: "Smith",
+  given_name: "John",
+  locale: "en",
+  name: "John Smith",
+  picture:
+    "https://lh3.googleusercontent.com/-ASaY0SANelo/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rdokkdjrZohTqzdndENxux0gLpnug/photo.jpg",
+  sub: "118064446743336738788"
+};
+
+export const facebookAuth = {
+  email: "sampleEmail@gmail.com",
+  first_name: "John Smith",
+  id: "798237947",
+  last_name: "Smith",
+  name: "John Smith",
+  picture: {
+    height: 50,
+    is_silhouette: false,
+    url:
+      "https://platform-lookaside.fbsbx.com/platform/prof…=50&width=50&ext=1575832834&hash=AeTXazoZHCqZHOD3",
+    width: 50
+  },
+  token: "<YOURTOKEN>"
+};
