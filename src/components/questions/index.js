5543c80b02076000d173a4eeab99fe9a134ef674
Jeremy Brady <jbdigitalgeek@gmail.com>
2019-11-08 13:18:36 -0800

    added fe tie in for testing adding of questions

---
+++ src/components/questions/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const addQuestionsCanvas = () => {
+    <>
+    <form>
+        <input type="text"/>
+        <input type="text"/>
+
+        <button>Submit</button>
+
+    </form>
+    </>
+}
+
+export default addQuestionsCanvas;
--- src/components/register/index.js
+++ src/components/register/index.js
@@ -4,11 +4,13 @@ import GithubSignInButton from './GithubSignInButton'
 import GoogleSignInButton from './GoogleSignInButton'
 import FacebookSignInButton from './FacebookSignInButton'
 import LocalSignUpForm from './LocalSignUpForm'
+import AddQuestion from '../questions/index'
 const SignInCanvas = () =>{
     console.log('herehere')
     return(
         <div className="SignInCanvas">
              <h1>Do things Here</h1>
+             <AddQuestion /><br />
             <GithubSignInButton/><br />
             <GoogleSignInButton/><br />
             <FacebookSignInButton/><br />
