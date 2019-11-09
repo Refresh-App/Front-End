cb5015d3f04cb388901cdc00a02dbd26fb7835e2
roman-jordan <rjordan1352@gmail.com>
2019-11-08 21:22:04 -0800

    yo

--- src/components/questions/NewQuestionForm.js
+++ src/components/questions/NewQuestionForm.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { routeStyle } from "../style";
-const NewQuestionForm = () => {
+const NewQuestionForm = props => {
   return (
     <div style={{ ...routeStyle }}>
     <p>Add a Question</p>
       <form>
-        <input type="text" placeholder="Object Key"/>
-        <input type="text" placeholder="Question"/>
+        <input type="text" placeholder="Object Key" disabled={!props.user && 'disable'}/>
+        <input type="text" placeholder="Question" disabled={!props.user && 'disable'}/>
 
         <button>Submit</button>
       </form>
