905091237a8e4ac5498a7425f46cd9743a395733
roman-jordan <rjordan1352@gmail.com>
2019-11-08 16:18:53 -0800

    yo e

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import SignInCanvas from './components/register/';
 import QuestionsCanvas from './components/questions/'
-
+import ProfileCanvas from './components/profile/'
 function App() {
   return (
     <>
       <SignInCanvas />
       <QuestionsCanvas />
+      <ProfileCanvas />
     </>
   );
 }
