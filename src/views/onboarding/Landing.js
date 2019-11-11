c86fe03800ef0187801e6c290b6cf3ff9f33e479
roman-jordan <rjordan1352@gmail.com>
2019-11-11 10:19:54 -0800

    Updated Models

--- src/views/onboarding/Landing.js
+++ src/views/onboarding/Landing.js
@@ -19,7 +19,7 @@ const Landing = (props) => {
         <OnBoardContainer>
             <Logo src={onboardlogo} />
             <FlexHolder>
-                <Refresh>Refres</Refresh>
+                <Refresh>Refresing</Refresh>
                 <OnboardTxt>Made for students,<br /> by students.</OnboardTxt>
                 <br />
                 <OnboardTxt>Because success starts with the <br /> body in mind.</OnboardTxt>
