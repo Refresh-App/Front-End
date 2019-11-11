ce5dbd30b9df0808fa134c3edeb7ce837a1558fe
roman-jordan <rjordan1352@gmail.com>
2019-11-11 10:10:55 -0800

    good to go

--- src/views/onboarding/Landing.js
+++ src/views/onboarding/Landing.js
@@ -19,7 +19,7 @@ const Landing = (props) => {
         <OnBoardContainer>
             <Logo src={onboardlogo} />
             <FlexHolder>
-                <Refresh>Check Yo</Refresh>
+                <Refresh>Check Yos</Refresh>
                 <OnboardTxt>Made for students,<br /> by students.</OnboardTxt>
                 <br />
                 <OnboardTxt>Because success starts with the <br /> body in mind.</OnboardTxt>
